e270f9db8520efe249039c21924666d02dae02c7
vinymeuh <vinymeuh@gmail.com>
2020-01-13 21:37:01 +0100

    fix testprog

--- weh001602a/testprog/main.go
+++ weh001602a/testprog/main.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	"periph.io/x/periph/conn/gpio/gpioreg"
@@ -15,38 +16,26 @@ import (
 	"github.com/vinymeuh/radiogagad/weh001602a"
 )
 
+var display *weh001602a.Display
+
+const displayerWidth = 16
+
 func centred(txt string) string {
-	padLen := int((weh001602a.Width - len(txt)) / 2)
-	return fmt.Sprintf("%*s%s%*s", padLen, "", txt, padLen, "")[0 : weh001602a.Width-1]
+	padLen := int((displayerWidth - len(txt)) / 2)
+	return fmt.Sprintf("%*s%s%*s", padLen, "", txt, padLen, "")[0 : displayerWidth-1]
 }
 
 func rightAligned(txt string) string {
-	return fmt.Sprintf("%*s", weh001602a.Width, txt)
+	return fmt.Sprintf("%*s", displayerWidth, txt)
 }
 
-func main() {
-	host.Init()
-	display, _ := weh001602a.NewDisplay(
-		gpioreg.ByName("GPIO7"),
-		gpioreg.ByName("GPIO8"),
-		gpioreg.ByName("GPIO25"),
-		gpioreg.ByName("GPIO24"),
-		gpioreg.ByName("GPIO23"),
-		gpioreg.ByName("GPIO27"),
-	)
-
-	// Send some centred test
+func writeAccentedCharacters() {
 	display.Clear()
-	display.Line1().Write(centred("Rasbperry Pi"))
-	display.Line2().Write(centred(":)"))
+	display.Line1().Write("àâäéèêëïîôöùûüÿç")
 	time.Sleep(3 * time.Second)
+}
 
-	// Send some left & right justified text
-	display.Clear()
-	display.Line1().Write("<- left")
-	display.Line2().Write(rightAligned("right ->"))
-	time.Sleep(3 * time.Second)
-
+func writeCustomCharacters() {
 	// Create & use custom characters
 	bell := [8]uint8{
 		0b00100,
@@ -98,26 +87,90 @@ func main() {
 	display.Clear()
 	display.Line1().WriteChar(0).Write(" ALARM")
 	display.Line2().WriteChar(1).Write(" BATTERY")
-	time.Sleep(3 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	display.Clear()
 	display.Line1().WriteChar(2).Write(" Pause")
 	display.Line2().WriteChar(3).Write(" Stop")
-	time.Sleep(3 * time.Second)
+	time.Sleep(2 * time.Second)
+}
 
-	// Write accented characters
-	display.Clear()
-	display.Line1().Write("àâäéèêëïîôöùûüÿç")
-	time.Sleep(3 * time.Second)
-
-	// Write characters font table
+func writeFontTable() {
 	for i := 0; i <= 255; i++ {
 		display.Clear()
 		display.Line1().Write(strconv.Itoa(i))
 		display.Line2().WriteChar(uint8(i))
 		time.Sleep(1 * time.Second)
 	}
+}
+
+type line struct {
+	txt    string
+	isLong bool
+}
+
+func main() {
+	host.Init()
+	display, _ = weh001602a.NewDisplay(
+		gpioreg.ByName("GPIO7"),
+		gpioreg.ByName("GPIO8"),
+		gpioreg.ByName("GPIO25"),
+		gpioreg.ByName("GPIO24"),
+		gpioreg.ByName("GPIO23"),
+		gpioreg.ByName("GPIO27"),
+	)
+
+	// Send some centred test
+	display.Clear()
+	display.Line1().Write(centred("Raspberry Pi"))
+	display.Line2().Write(centred(":)"))
+	time.Sleep(2 * time.Second)
+
+	// Send some left & right justified text
+	display.Clear()
+	display.Line1().Write("<- left")
+	display.Line2().Write(rightAligned("right ->"))
+	time.Sleep(2 * time.Second)
+
+	writeAccentedCharacters()
+	writeCustomCharacters()
+	//writeFontTable()
+
+	display.Clear()
+	var w sync.WaitGroup
+	w.Add(1)
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		display.Line1().Write("Scrolling")
+		msg2 := "I'm scrolling from right to left"
+		line2 := fmt.Sprintf("%s                %s", msg2, msg2)
+		l2 := len(line2)
+		s2 := 0
+		e2 := displayerWidth - 1
+		tick := 0
+		for {
+			select {
+			case <-ticker.C:
+				if tick == 60 {
+					w.Done()
+					return
+				}
+				tick++
+				display.Line2().Write(line2[s2:e2])
+				s2 = (s2 + 1) % l2
+				e2 = s2 + (displayerWidth - 1)
+				if e2 > l2 {
+					e2 = l2
+				}
+			}
+		}
+	}()
+	w.Wait()
 
 	// Shutdown
+	display.Clear()
+	display.Line1().Write("Shutdown")
+	time.Sleep(2 * time.Second)
+	display.Clear()
 	display.Off()
 }
