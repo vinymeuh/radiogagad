98ff0c4bf9586cfd391ca17b9d964a1f53168b95
vinymeuh <vinymeuh@gmail.com>
2020-04-03 19:30:59 +0200

    displayer: code cleaning

--- displayer.go
+++ displayer.go
@@ -43,10 +43,10 @@ var (
 
 // Displayer manages the display, mainly showing MPD messages received from MPDClient.fetcher
 type Displayer struct {
-	Chip    string       `yaml:"chip"`
-	Lines   DisplayLines `yaml:"lines"`
-	Width   int          `yaml:"line_width"`
-	display *weh001602a.Display
+	Chip    string              `yaml:"chip"`
+	Lines   DisplayLines        `yaml:"lines"`
+	Width   int                 `yaml:"line_width"`
+	display *weh001602a.Display // useful to commands impacting both lines (Clear, Off)
 	line1   line
 	line2   line
 	refresh bool // controls screen refresh
@@ -63,25 +63,39 @@ type DisplayLines struct {
 }
 
 type line struct {
-	txt    string
-	length int
-	start  int
-	end    int
+	disp      *weh001602a.Display
+	posCursor func() *weh001602a.Display
+	width     int
+	txt       string
+	length    int
+	start     int
+	end       int
 }
 
-func (l *line) reset() {
-	l.txt = ""
-	l.length = 0
+func (l *line) setTxt(txt string) {
+	if len(txt) < l.width-1 {
+		padding := int((l.width - len(txt)) / 2)
+		l.txt = fmt.Sprintf("%*s%s%*s", padding, "", txt, padding, "")[0 : l.width-1]
+	} else {
+		l.txt = fmt.Sprintf("%s                %s", txt, txt[0:l.width-1])
+	}
+	l.length = len(l.txt)
 	l.start = 0
-	l.end = 0
+	l.end = l.width - 1
 }
 
-func (l *line) scroll(lineWidth int) {
+func (l *line) refresh() {
+	if l.length == l.width {
+		l.posCursor().Write(l.txt)
+		return
+	}
+	// scrolling
+	l.posCursor().Write(l.txt[l.start:l.end])
 	l.start++
 	l.end++
 	if l.end > l.length {
-		l.start = 1
-		l.end = lineWidth
+		l.start = 0
+		l.end = l.width - 1
 	}
 }
 
@@ -94,6 +108,15 @@ func (d Displayer) start(chip chardevgpio.Chip, mpdinfo chan mpdInfo, stopscr ch
 		msgch <- fmt.Sprintf("Failed to setup weh001602a display: %v", err)
 		return
 	}
+
+	d.line1.width = d.Width
+	d.line1.disp = d.display
+	d.line1.posCursor = d.display.Line1
+
+	d.line2.width = d.Width
+	d.line2.disp = d.display
+	d.line2.posCursor = d.display.Line2
+
 	clrscr.Add(1)
 
 	d.display.CreateChar(cPause, glyphPause)
@@ -101,7 +124,8 @@ func (d Displayer) start(chip chardevgpio.Chip, mpdinfo chan mpdInfo, stopscr ch
 
 	// greeting message
 	d.display.Clear()
-	d.display.Line1().Write(d.fmtCentred("Hello"))
+	d.line1.setTxt("Hello")
+	d.line1.refresh()
 	time.Sleep(2 * time.Second)
 
 	// main
@@ -112,23 +136,11 @@ func (d Displayer) start(chip chardevgpio.Chip, mpdinfo chan mpdInfo, stopscr ch
 			//-- refresh screen --//
 			case <-ticker.C:
 				if d.refresh == true {
-					if d.line1.length > 0 {
-						d.display.Line1().Write(d.line1.txt[d.line1.start:d.line1.end])
-						d.line1.scroll(d.Width)
-					}
-
-					if d.line2.length > 0 {
-						d.display.Line2().Write(d.line2.txt[d.line2.start:d.line2.end])
-						d.line2.scroll(d.Width)
-					}
+					d.line1.refresh()
+					d.line2.refresh()
 				}
-			//-- shutdown --//
-			case <-stopscr:
-				d.shutdown()
-				clrscr.Done()
-				return
 
-			//-- display info retrieves from mpdinfo --//
+			//-- retrieves info from mpdinfo --//
 			case data := <-mpdinfo:
 				switch data.State {
 				case "play":
@@ -137,68 +149,39 @@ func (d Displayer) start(chip chardevgpio.Chip, mpdinfo chan mpdInfo, stopscr ch
 					switch data.File[0:4] {
 					case "http":
 						msgch <- fmt.Sprintf("Playing radio='%s', title='%s'", data.Name, data.Title)
-						d.line1.txt = data.Name
-						d.line2.txt = data.Title
+						d.line1.setTxt(data.Name)
+						d.line2.setTxt(data.Title)
 					default:
 						msgch <- fmt.Sprintf("Playing artist='%s', album='%s', title='%s', %d/%d\n",
 							data.Artist, data.Album, data.Title, data.Song+1, data.PlaylistLength)
-						d.line1.txt = data.Artist
-						d.line2.txt = data.Title
+						d.line1.setTxt(data.Artist)
+						d.line2.setTxt(data.Title)
 					}
-					// write line1 & line2
-					d.display.Clear()
+					// display refresh is delayed to next tick
 
-					if len(d.line1.txt) < d.Width-1 {
-						d.line1.length = 0
-						d.display.Line1().Write(d.fmtCentred(d.line1.txt))
-					} else {
-						d.line1.txt = fmt.Sprintf("%s                %s", d.line1.txt, d.line1.txt[0:d.Width-1])
-						d.line1.length = len(d.line1.txt)
-						d.line1.start = 0
-						d.line1.end = d.Width - 1
-						// display delayed to next tick
-					}
-
-					if len(d.line2.txt) < d.Width-1 {
-						d.line2.length = 0
-						d.display.Line2().Write(d.fmtCentred(d.line2.txt))
-					} else {
-						d.line2.txt = fmt.Sprintf("%s                %s", d.line2.txt, d.line2.txt[0:d.Width-1])
-						d.line2.length = len(d.line2.txt)
-						d.line2.start = 0
-						d.line2.end = d.Width - 1
-						// display delayed to next tick
-					}
 				case "pause":
-					d.refresh = false
 					msgch <- "Player paused"
-					d.line1.reset()
-					d.line2.reset()
-					d.display.Clear()
-					d.display.Line1().Write(d.fmtCentred("Pause"))
+					d.line1.setTxt("Pause")
+					d.line2.setTxt("")
 
 				default:
-					d.refresh = false
 					msgch <- "Player stopped"
-					d.line1.reset()
-					d.line2.reset()
-					d.display.Clear()
-					d.display.Line1().Write(d.fmtCentred("Stop"))
+					d.line1.setTxt("Stop")
+					d.line2.setTxt("")
 				}
+
+			//-- shutdown requested --//
+			case <-stopscr:
+				d.refresh = false
+				d.display.Clear()
+				d.line1.setTxt("Bye Bye")
+				d.line1.refresh()
+				time.Sleep(2 * time.Second)
+				d.display.Clear()
+				d.display.Off()
+				clrscr.Done()
+				return
 			}
 		}
 	}()
-}
-
-func (d Displayer) shutdown() {
-	d.display.Clear()
-	d.display.Line1().Write(d.fmtCentred("Bye Bye"))
-	time.Sleep(2 * time.Second)
-	d.display.Clear()
-	d.display.Off()
-}
-
-func (d Displayer) fmtCentred(txt string) string {
-	padLen := int((d.Width - len(txt)) / 2)
-	return fmt.Sprintf("%*s%s%*s", padLen, "", txt, padLen, "")[0 : d.Width-1]
 }
